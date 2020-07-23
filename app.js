5acc600854598c6a89c7a70582c0fde8cdc4a591
icedike <icedike@gmail.com>
2020-07-23 22:40:23 +0800

    project init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exhbps = require('express-handlebars')
+
+const app = express()
+
+const port = 3000
+
+app.engine('handlebars', exhbps({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log('Running server')
+})
