2c1347b3ae15786538f58dcf33fc9c9af0308d48
icedike <icedike@gmail.com>
2020-07-24 15:32:01 +0800

    Basic layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exhbps = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -7,8 +8,14 @@ const port = 3000
 
 app.engine('handlebars', exhbps({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
   res.render('index')
 })
 
